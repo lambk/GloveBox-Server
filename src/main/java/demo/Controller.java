6832232e7143026a448f42e747bded4cc780580a
lambk <kyle.lamb@hotmail.co.nz>
2018-08-25 12:57:53 +1200

    Testing local and prod environment configurations

--- src/main/java/demo/Controller.java
+++ src/main/java/demo/Controller.java
@@ -1,14 +1,20 @@
 package demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+    @Value("${test}")
+    private String ok;
 
     @RequestMapping("/person")
     public Person person(@RequestParam(value="name") String name) {
-        return new Person(1, name);
+        if (ok == null) {
+            ok = System.getenv("test");
+        }
+        return new Person(1, ok);
     }
 }
